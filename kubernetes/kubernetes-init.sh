2f705b24a2c8baa768c4698417fc6a5648394ec9
Matthew Johnson <mjohnson@omega-fortytwo.local>
2021-02-21 21:22:55 -0600

    Fix typo in docker daemon configuration

--- kubernetes/kubernetes-init.sh
+++ kubernetes/kubernetes-init.sh
@@ -9,16 +9,8 @@ USER_HOME=$1
 echo
 echo "[INFO] starting install of kubernetes as microk8s"
 echo
-# add docker configuration
-cat << _EOF > ${USER_HOME}/daemon.json
-cat
-{
-  "insecure-registries" : ["localhost:32000"]
-}
-_EOF
-mv ${USER_HOME}/daemon.json /etc/docker/daemon.json
-systemctl restart docker
 # install microk8s to allow for kubernetes master (control plane) and slave (node) on a single host
+# microk8s=1.20 has same transitive dependency, containerd=1.3.7, as docker=19.0.13
 snap install --classic --channel=1.20/stable microk8s
 sudo usermod -a -G microk8s ubuntu
 echo
@@ -26,6 +18,13 @@ echo "[INFO] beginning configuration of minikube"
 echo
 # configure microk8s
 microk8s enable dns registry storage ingress
+cat << _EOF > ${USER_HOME}/daemon.json
+{
+  "insecure-registries" : ["localhost:32000"]
+}
+_EOF
+mv ${USER_HOME}/daemon.json /etc/docker/daemon.json
+systemctl restart docker
 echo
 echo "[INFO] kubernetes install complete"
 echo
